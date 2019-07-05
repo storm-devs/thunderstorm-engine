581819c74e676c2f39755d7b5da27418f5b9b336
espkk <espkk@protonmail.ch>
2019-07-05 22:11:02 +0300

    cleanup

--- Battle_interface/spyglass/spyglass.h
+++ Battle_interface/spyglass/spyglass.h
@@ -5,6 +5,7 @@
 #include "../../Common_h/defines.h"
 #include <string>
 #include <vector>
+#include "../../Common_h/Entity.h"
 
 class BIImageRender;
 class BIImage;
@@ -66,6 +67,21 @@ public:
 	void Execute(uint32_t delta_time);
 	void Realize(uint32_t delta_time);
     uint32_t ProcessMessage(MESSAGE & message);
+	void ProcessStage(Stage stage, uint32_t delta) override
+	{
+		switch (stage)
+		{
+		case Stage::EXECUTE:
+			Execute(delta); break;
+		case Stage::REALIZE:
+			Realize(delta); break;
+			/*case Stage::LOST_RENDER:
+				LostRender(delta); break;
+			case Stage::RESTORE_RENDER:
+				RestoreRender(delta); break;*/
+		}
+	}
+
 
 protected:
 	VDX9RENDER *rs;
